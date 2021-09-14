cd109f7e442b08b574901dbfcf0a07b94609a445
hanyucd <1807003141@qq.com>
2021-09-14 20:34:02 +0800

    style: 优化

--- service/userService.js
+++ service/userService.js
@@ -8,7 +8,18 @@ class UserService {
    * @param {String} password 密码
    */
   async getUserInfo(userName, password) {
+    let queryObj = { userName };
 
+    if (password) queryObj = { ...queryObj, password };
+
+    const queryRes = await userModel.findOne({
+      where: queryObj,
+      attributes: ['id', 'userName', 'nickName', 'picture', 'gender', 'city']
+    });
+
+    if (!queryRes) return null;
+
+    return queryRes;
   }
 }
 
